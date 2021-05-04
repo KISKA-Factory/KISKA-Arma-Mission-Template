c5e9891b72ddddca16e5dbee9e942e44f3a5bc62
Ansible2 <afoshark@yahoo.com>
2021-05-04 10:44:56 -0600

    Added tasks

--- Headers/descriptionEXT/KISKA Tasks.hpp
+++ Headers/descriptionEXT/KISKA Tasks.hpp
@@ -1,15 +1,15 @@
 #define ON 1
 #define OFF 0
 
-class KISKA_Tasks
+class KISKA_cfgTasks
 {
-    class exampleTask
+    class exampleTask_base // class name will become Task Id
     {
-        title = "";
-        description = "";
+        title = "My Example Task";
+        description = "This is an example";
 
-        parentTask = "";
-        type = "";
+        parentTask = ""; // Parent Task Id
+        type = ""; // task type as defined in CfgTaskTypes
 
         // params for these are:
         // 0: task id (or class name) (STRING)
@@ -17,6 +17,46 @@ class KISKA_Tasks
         // 2: task state (STRING)
         onComplete = ""; // code that runs upon completion of task when using KISKA_fnc_endTask
         onCreate = ""; // code that runs on creation of task when using KISKA_fnc_createTaskFromConfig
+
+        destination[] = {}; // position of task
+
+        defaultState = ""; // "CREATED", "ASSIGNED", "AUTOASSIGNED" (default), "SUCCEEDED", "FAILED", or "CANCELED"
+        priority = -1;
+        notifyOnComplete = ON;
+        notifyOnCreate = ON;
+
+        visibleIn3D = OFF; // 3d marker creation
+    };
+    class exampleTask_parent : exampleTask_base
+    {
+        title = "My Example Task Parent";
+        description = "This is an example task parent";
+
+        parentTask = "";
+        type = "default";
+
+        onComplete = "hint 'Parent complete'";
+        onCreate = "hint 'Parent created'";
+
+        destination[] = {};
+
+        defaultState = "";
+        priority = -1;
+        notifyOnComplete = ON;
+        notifyOnCreate = ON;
+
+        visibleIn3D = OFF;
+    };
+    class exampleTask_child : exampleTask_base
+    {
+        title = "My Example Task Child";
+        description = "This is an example task child";
+
+        parentTask = "exampleTask_parent";
+        type = "Move";
+
+        onComplete = "hint 'Child complete'";
+        onCreate = "hint 'Child created'";
 
         destination[] = {};
 
