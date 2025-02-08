a39fd6d45c23a17e4a0874f4f189b3b37574168a
Ansible2 <afoshark@yahoo.com>
2025-02-07 20:14:32 -0700

    Simplified examples and added separate files

---
+++ Headers/descriptionEXT/RespawnInventory/CfgRoles.hpp
@@ -0,0 +1,9 @@
+// https://community.bistudio.com/wiki/Description.ext#CfgRoles
+
+/*
+class MyRole
+{
+    displayName = "MyRole";
+    icon = ""; // path to icon such as "\A3\Ui_f\data\GUI\Cfg\Ranks\sergeant_gs.paa"
+};
+*/
