c8d536c5a1dca9d4d33a30a751e77cfb9797215e
Ansible2 <afoshark@yahoo.com>
2025-05-11 13:53:27 -0600

    Added basics of KISKA_supportDetails

--- Headers/descriptionEXT/supports.hpp
+++ Headers/descriptionEXT/supports.hpp
@@ -1,72 +1,227 @@
-#if __has_include("KISKA Systems\Headers\Support Framework\basic supports.hpp")
-    #include "KISKA Systems\Headers\Support Framework\basic supports.hpp"
-#else
-    #if __has_include("\KISKA_Functions\Headers\Support Framework\basic supports.hpp")
-        // import KISKA_basicSupport_baseClass from CfgCommunicationMenu;
-        // import KISKA_artillery_baseClass from CfgCommunicationMenu;
-        // import KISKA_CAS_baseClass from CfgCommunicationMenu;
-        // import KISKA_attackHelicopterCAS_baseClass from CfgCommunicationMenu;
-        // import KISKA_helicopterCAS_baseClass from CfgCommunicationMenu;
-        // import KISKA_arsenalSupplyDrop_baseClass from CfgCommunicationMenu;
-        // import KISKA_ARTY_155_templateClass from CfgCommunicationMenu;
-        // import KISKA_ARTY_120_templateClass from CfgCommunicationMenu;
-        // import KISKA_ARTY_82_templateClass from CfgCommunicationMenu;
-        // import KISKA_ARTY_230_templateClass from CfgCommunicationMenu;
-        // import KISKA_supplyDrop_aircraft_baseClass from CfgCommunicationMenu;
-    #endif
+INCLUDE_IF_HAS("\Kiska_Functions\Headers\Support Framework\CommonSupportConfigMacros.hpp")
 
-    #if __has_include("\KISKA_Functions\Headers\Support Framework\Support Type IDs.hpp")
-        #include "\KISKA_Functions\Headers\Support Framework\Support Type IDs.hpp"
-    #endif
-
-    #if __has_include("\KISKA_Functions\Headers\Support Framework\Support Icons.hpp")
-        #include "\KISKA_Functions\Headers\Support Framework\Support Icons.hpp"
-    #endif
-
-    #if __has_include("\KISKA_Functions\Headers\Support Framework\CAS Type IDs.hpp")
-        #include "\KISKA_Functions\Headers\Support Framework\CAS Type IDs.hpp"
-    #endif
-
-    #if __has_include("\KISKA_Functions\Headers\Support Framework\Arty Ammo Type IDs.hpp")
-        #include "\KISKA_Functions\Headers\Support Framework\Arty Ammo Type IDs.hpp"
-    #endif
-
-    /*
-        This master function for supports is used as go between for error cases such as when
-        a player provides an invalid position (looking at the sky). It will then refund the
-        support back to the player.
-    */
-    #define CALL_SUPPORT_MASTER(CLASS) "["#CLASS",_this,%1] call KISKA_fnc_callingForSupportMaster"
-    #define EXPRESSION_CALL_MASTER(CLASS) expression = CALL_SUPPORT_MASTER(CLASS);
-
-    /*
-    // expression arguments
-
-    [caller, pos, target, is3D, id]
-        caller: Object - unit which called the item, usually player
-        pos: Array in format Position - cursor position
-        target: Object - cursor target
-        is3D: Boolean - true when in 3D scene, false when in map
-        id: String - item ID as returned by BIS_fnc_addCommMenuItem function
-    */
-
-    /*
-        if a class is to be solely a base one, you need to include _baseClass (EXACTLY AS IT IS CASE SENSITIVE)
-        somewhere in the class name so that it can be excluded from being added to the shop
-    */
-    /*
-    class KISKA_basicSupport_baseClass
+class KISKA_abstract_commMenuSupport
+{
+    class KISKA_commMenuDetails
     {
-        text = "I'm a support!"; // text in support menu
-        expression = ""; // code to compile upon call of menu item
-        icon = CALL_ICON; // icon in support menu
-        removeAfterExpressionCall = 1;
-
-        supportTypeId = SUPPORT_TYPE_CUSTOM;
-
-        // used for support selection menu
-        // _this select 0 is the classname
-        managerCondition = "";
     };
-    */
-#endif
+
+    // Governs the support framework interaction
+    // This is an essential class
+    class KISKA_supportDetails
+    {
+        /* -------------------------------------------------------------------------------
+            Description: 
+                - numberOfUses: <NUMBER> - The number of "uses" that the support has in total.
+                    For things like artillery, it could correspond to how many rounds in
+                    total can be fired.
+
+            Required: 
+                - YES
+
+            Examples:
+                (begin example)
+                    numberOfUses = 1;
+                (end)
+        ------------------------------------------------------------------------------- */
+        numberOfUses = 1;
+
+        /* -------------------------------------------------------------------------------
+            Description: 
+                - onSupportAdded: <STRING> - Uncompiled code that will be compiled and executed
+                immediatley after the support is added. Only executed on the local machine.
+                Most commonly will be used with `KISKA_fnc_commMenu_onSupportAdded`.
+                See `KISKA_fnc_supports_add` for further detail.
+
+                Parameters:
+                    0: <STRING> - The id of the added support.
+                    1: <CONFIG> - The top level config of the support, meaning the parent of 
+                        this `KISKA_supportDetails` class.
+                    2: <NUMBER> - How many uses the support currently has left.
+
+            Required: 
+                - NO
+
+            Examples:
+                (begin example)
+                    onSupportAdded = "_this call KISKA_fnc_commMenu_onSupportAdded";
+                (end)
+        ------------------------------------------------------------------------------- */
+        onSupportAdded = "_this call KISKA_fnc_commMenu_onSupportAdded";
+
+        /* -------------------------------------------------------------------------------
+            Description: 
+                - onSupportRemoved: <STRING> - Uncompiled code that will be compiled and executed
+                immediatley after the support is removed. Only executed on the local machine.
+                Most commonly will be used with `KISKA_fnc_commMenu_onSupportRemoved`.
+                See `KISKA_fnc_supports_remove` for further detail.
+
+                Parameters:
+                    0: <STRING> - The id of the removed support.
+                    1: <CONFIG> - The top level config of the support, meaning the parent of 
+                        this `KISKA_supportDetails` class.
+
+            Required: 
+                - YES
+
+            Examples:
+                (begin example)
+                    onSupportRemoved = "_this call KISKA_fnc_commMenu_onSupportRemoved";
+                (end)
+        ------------------------------------------------------------------------------- */
+        onSupportRemoved = "_this call KISKA_fnc_commMenu_onSupportRemoved";
+
+        /* -------------------------------------------------------------------------------
+            Description: 
+                - onSupportCalled: <STRING> - Uncompiled code that will be compiled and executed
+                immediatley after the support is called. Only executed on the local machine.
+                This should handle the producing the actual effect of the support like launching
+                artillery shells or conducting an aircraft to shoot, etc.
+
+                See `KISKA_fnc_supports_call` for further detail.
+
+                Parameters:
+                    0: <ANY> - Whatever arguments are passed to `KISKA_fnc_supports_call` as the
+                        on called args.
+                    1: <STRING> - The id of the called support.
+                    2: <CONFIG> - The top level config of the support, meaning the parent of 
+                        this `KISKA_supportDetails` class.
+                    3: <PositionASL[]> - The targetted position of the support.
+                    4: <NUMBER> - How many uses of the support were used in the call.
+
+            Required: 
+                - YES
+
+            Examples:
+                (begin example)
+                    onSupportCalled = "_this call KISKA_fnc_supports_onCalledVirtualArty";
+                (end)
+        ------------------------------------------------------------------------------- */
+        onSupportCalled = "";
+
+        // TODO:
+        /*
+        class AddedNotifcation
+        {
+
+        };
+        */
+    };
+
+    // Governs interaction with the KISKA support manager
+    // If the support is not intended for use in it, remove the class
+    class KISKA_supportManagerDetails
+    {
+        /* -------------------------------------------------------------------------------
+            Description: 
+                - text: <STRING> - The text that will appear in the KISKA Support manager
+                    lists for this support.
+
+            Required: 
+                - YES
+
+            Examples:
+                (begin example)
+                    text = "Custom Support";
+                (end)
+        ------------------------------------------------------------------------------- */
+        text = "My Support";
+
+        /* -------------------------------------------------------------------------------
+            Description: 
+                - picture: <STRING> - A path to an icon that will display to the left of the
+                    text in the Support Manager.
+
+            Required: 
+                - NO
+
+            Examples:
+                (begin example)
+                    picture = CALL_ICON;
+                (end)
+        ------------------------------------------------------------------------------- */
+        // picture = "";
+
+        /* -------------------------------------------------------------------------------
+            Description: 
+                - pictureColor: <ARRAY> - An array of RGBA that will determine what color
+                    the support is in the manager (when it has not been used before).
+
+            Required: 
+                - NO
+
+            Examples:
+                (begin example)
+                    pictureColor[] = {1,0,1,1};
+                (end)
+        ------------------------------------------------------------------------------- */
+        // pictureColor[] = {};
+
+        /* -------------------------------------------------------------------------------
+            Description: 
+                - selectedPictureColor: <ARRAY> - An array of RGBA that will determine what color
+                    the support is in the manager (when it has not been used before) and it
+                    is currently selected in a listbox.
+
+            Required: 
+                - NO
+
+            Examples:
+                (begin example)
+                    selectedPictureColor[] = {1,0,1,1};
+                (end)
+        ------------------------------------------------------------------------------- */
+        // selectedPictureColor[] = {};
+
+        /* -------------------------------------------------------------------------------
+            Description: 
+                - tooltip: <STRING> - Tooltip text that will appear when hovering over
+                    a support of this class in the manager.
+
+            Required: 
+                - NO
+
+            Examples:
+                (begin example)
+                    tooltip = "hello tooltip";
+                (end)
+        ------------------------------------------------------------------------------- */
+        // tooltip = "";
+
+        /* -------------------------------------------------------------------------------
+            Description: 
+                - managerCondition: <STRING> - Code that will be compiled and run when
+                    a player attempts to take the support from the support manager.
+                    Must return a <BOOL>, `true` means that the user can take the support,
+                    `false` means that they cant.
+
+                    Parameters:
+                        0: <CONFIG> - The parent config above the KISKA_supportManagerDetails
+
+            Required: 
+                - NO
+
+            Examples:
+                (begin example)
+                    managerCondition = "params ["_supportConfig"]; true";
+                (end)
+        ------------------------------------------------------------------------------- */
+        // managerCondition = "";
+
+        /* -------------------------------------------------------------------------------
+            Description: 
+                - conditionMessage: <STRING> - A message that will appear when the attempting
+                    to take the support results in a failure. A default message is used in
+                    the event this is undefined or `""`.
+
+            Required: 
+                - NO
+
+            Examples:
+                (begin example)
+                    conditionMessage = "You cant take the support";
+                (end)
+        ------------------------------------------------------------------------------- */
+        // conditionMessage = "";
+    };
+};
